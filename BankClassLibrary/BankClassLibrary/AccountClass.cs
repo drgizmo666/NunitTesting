6003a011fbdeb88cac0c89ddec56de1c23e3728d
drgizmo666 <james_e_powell@live.com>
2016-02-08 15:18:43 -0800

    Bank Class Library
    added the bank class library

---
+++ BankClassLibrary/BankClassLibrary/AccountClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BankClassLibrary
+{
+    public class AccountClass
+    {
+        private decimal balance;
+
+        public void Deposit(decimal amount)
+        {
+            balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            balance -= amount;
+        }
+
+        public void TransferFunds(AccountClass destination, decimal amount)
+        {
+            
+
+            if (balance - amount < minimumBalance)
+                throw new InsufficientFundsException();
+
+            destination.Deposit(amount);
+
+            Withdraw(amount);
+        }
+
+        public decimal Balance { get { return balance; } }
+
+        private decimal minimumBalance = 10m;
+        public decimal MinimumBalance
+        {
+            get { return minimumBalance; }
+        }
+
+        public class InsufficientFundsException : ApplicationException
+        {
+
+        }
+    }
+}
---
+++ BankClassLibrary/BankClassLibrary/AccountTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BankClassLibrary
+{
+    [TestFixture]
+    class AccountTest
+    {
+        AccountClass source;
+        AccountClass destination;
+
+        [SetUp]
+        public void Init()
+        {
+            source = new AccountClass();
+            source.Deposit(200m);
+            destination = new AccountClass();
+            destination.Deposit(150m);
+        }
+
+        [Test]
+        public void TransferFunds()
+        {
+            source.TransferFunds(destination, 100m);
+
+            Assert.AreEqual(250m, destination.Balance);
+            Assert.AreEqual(100m, source.Balance);
+        }
+
+        [Test]
+        [ExpectedException(typeof(BankClassLibrary.AccountClass.InsufficientFundsException))]
+        public void TransferWithInsufficientFunds()
+        {
+            source.TransferFunds(destination, 300m);
+        }
+
+        [Test]
+        [Ignore("Decide how to implement transaction management")]
+        public void TransferWithInsufficientFundsAtomicity()
+        {
+            try
+            {
+                source.TransferFunds(destination, 300m);
+            }
+            catch (BankClassLibrary.AccountClass.InsufficientFundsException expected)
+            {
+            }
+
+            Assert.AreEqual(200m, source.Balance);
+            Assert.AreEqual(150m, destination.Balance);
+        }
+    }
+}
